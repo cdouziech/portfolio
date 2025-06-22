744e9416a84ea8dae4b08f4cecfeeb616d10c7b2
Clotaire Douziech <clotairedouziech@gmail.com>
2025-06-23 00:39:58 +0200

    Temp rename

---
+++ src/pages/errorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import './errorPage.scss'
+
+function ErrorPage(){
+    return(
+        <div className='Error'>
+            <h1>Erreur 404</h1>
+            <p>La page recherchée n'a pas été trouvée !</p>
+        </div>
+    )
+}
+
+export default ErrorPage;
